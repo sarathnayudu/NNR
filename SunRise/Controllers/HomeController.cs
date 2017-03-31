9b0dd9a29686fcec574c8bfb247d41286468741e
sarathnayudunz <sarath.nayudu@ntcsoftech.com>
2017-03-31 13:09:47 +0530

    added sunraise project

---
+++ SunRise/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SunRise.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult OurWork()
+        {
+           
+
+            return View("_OurWork");
+        }
+
+        public ActionResult Contact()
+        {
+           
+
+            return View("_Contact");
+        }
+
+        public ActionResult Projects()
+        {
+          
+
+            return View("_Projects");
+        }
+
+        public ActionResult Testimonials()
+        {
+          
+
+            return View("_Testimonials");
+        }
+    }
+}
---
+++ SunRise/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SunRise.Startup))]
+namespace SunRise
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
