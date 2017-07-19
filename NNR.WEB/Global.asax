181dad0839c32566a3f5547d822761661b6cc7f2
sarathnayudunz <sarath.nayudu@ntcsoftech.com>
2017-07-19 11:51:11 +0530

    07/19/2017- replace latest application

--- NNR.WEB/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="NNR.WEB.MvcApplication" Language="C#" %>
---
+++ NNR_Test/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(NNR_Test.Startup))]
+namespace NNR_Test
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
